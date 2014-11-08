b941342e56471669434fafc4bd7a0ac31ea4d279
Camilo Sanchez <camilin87@gmail.com>
2014-11-08 00:04:40 -0500

    declare simple factorial

---
+++ client/simpleFactorial.js
@@ -0,0 +1,3 @@
+var fact = function(){
+    return 4;
+};
---
+++ client/simpleFactorialSpec.js
@@ -0,0 +1,7 @@
+describe("simple factorial", function(){
+
+    it ("always return 4", function(){
+        expect(fact()).toBe(4);
+    });
+
+});
