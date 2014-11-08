0f22d6613d5d964a90d460f4edb3eaca64e51c8c
Camilo Sanchez <camilin87@gmail.com>
2014-11-08 00:14:53 -0500

    convert factorial into its own executable

--- server/factorial.py
+++ server/factorial.py
@@ -1,9 +1,13 @@
 from sys import argv
 
-def _fact(n):
+def fact(n):
     result = 1
     for i in xrange(2, n + 1):
         result = result * i
     return result
 
-print _fact(int(argv[1]))
+def main(args):
+    print fact(int(argv[1]))
+
+if __name__ == "__main__":
+    main(argv)
